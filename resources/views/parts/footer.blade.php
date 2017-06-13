705eff19f51923316848df0d26eada107f269ec6
Aidosgd <aidosgd@gmail.com>
2017-06-13 19:01:17 +0600

    fc

---
+++ resources/views/parts/footer.blade.php
@@ -0,0 +1,15 @@
+<!-- PAGE FOOTER -->
+<footer class="page-footer page-footer_multi">
+    <div class="container">
+        <div class="page-footer_multi__social">
+            <ul class="list-social list-social_large">
+                <li class="list-social__item"><a class="list-social__link fa fa-facebook" href="#"></a></li>
+                <li class="list-social__item"><a class="list-social__link fa fa-twitter" href="#"></a></li>
+                <li class="list-social__item"><a class="list-social__link fa fa-google-plus" href="#"></a></li>
+                <li class="list-social__item"><a class="list-social__link fa fa-linkedin" href="#"></a></li>
+            </ul>
+        </div>
+        <div class="page-footer_multi__copyright"><small class="page-footer_multi__copyright-text">Copyright © 2017 - Cleaner</small></div>
+    </div>
+</footer>
+<!-- - PAGE FOOTER -->
---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,106 @@
+<!-- PAGE HEADER -->
+<header class="page-header page-header_multi">
+    <!-- topbar -->
+    <div class="topbar-wrapper">
+        <div class="container">
+            <div class="topbar">
+                <div class="topbar__section">
+                    <ul class="list-contacts">
+                        <li class="list-contacts__item"><span class="list-contacts__label">Welcome to Cleaning Service Store</span></li>
+                    </ul>
+                </div>
+                <div class="topbar__section"><a class="button button_default button_colored-2" id="js-toggle-estimate" href="#">Quick estimate</a>
+                    <div class="page-header_multi__form-estimate" id="js-form-estimate">
+                        <form class="form form_estimate" action="home-01.html" method="GET">
+                            <div class="form-control form_estimate__control">
+                                <input class="form-control__input form_estimate__control-input" type="text" placeholder="Your Name">
+                            </div>
+                            <div class="form-control form_estimate__control">
+                                <input class="form-control__input form_estimate__control-input" type="email" placeholder="Email">
+                            </div>
+                            <div class="form-control form_estimate__control">
+                                <input class="form-control__input form_estimate__control-input" type="tel" placeholder="Phone Number">
+                            </div>
+                            <div class="form-control form_estimate__control">
+                                <select class="form-control__input form_estimate__control-input">
+                                    <option>Cleaning Services</option>
+                                    <option>Residental Cleaning</option>
+                                    <option>Construction Cleaning</option>
+                                    <option>Commercial Cleaning</option>
+                                    <option>Vehicle Cleaning</option>
+                                    <option>Office Cleaning</option>
+                                    <option>Wall Cleaning</option>
+                                </select>
+                            </div>
+                            <div class="form-control form_estimate__control">
+                                <input class="form-control__input form_estimate__control-input" type="text" placeholder="dd/mm/yyyy">
+                            </div>
+                            <div class="form-control form_estimate__control">
+                                <textarea class="form-control__input form_estimate__control-input form_estimate__control-textarea form-control__textarea" placeholder="Address"></textarea>
+                            </div>
+                            <div class="form-control form_estimate__control">
+                                <textarea class="form-control__input form_estimate__control-input form_estimate__control-textarea form-control__textarea" placeholder="Description"></textarea>
+                            </div>
+                            <div class="form__wrapper-submit form_estimate__wrapper-submit"><a class="form__submit-button button button_default button_large" href="#">Get quick estimate</a></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- - topbar -->
+    <!-- nav -->
+    <div class="top-navigation-wrapper">
+        <nav class="top-navigation">
+            <div class="top-navigation__logo"><a class="site-logo" href="home-02.html"><img class="site-logo__img" src="img/logo3.png" alt="Cleaner HTML Template"></a></div>
+            <div class="top-navigation__menu">
+                <div class="container">
+                    <ul class="list-top-menu">
+                        <li class="list-top-menu__item list-top-menu__item_has-submenu"><a class="list-top-menu__link" href="#">Home</a>
+                            <ul class="list-submenu">
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="index.html">Home v1</a></li>
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="home-02.html">Home v2</a></li>
+                            </ul>
+                        </li>
+                        <li class="list-top-menu__item list-top-menu__item_has-submenu"><a class="list-top-menu__link" href="#">About us</a>
+                            <ul class="list-submenu">
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="about.html">About Company</a></li>
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="about-history.html">Our History</a></li>
+                            </ul>
+                        </li>
+                        <li class="list-top-menu__item list-top-menu__item_has-submenu"><a class="list-top-menu__link" href="#">Services</a>
+                            <ul class="list-submenu">
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="services.html">Our Services</a></li>
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="service-details.html">Service Details</a></li>
+                            </ul>
+                        </li>
+                        <li class="list-top-menu__item list-top-menu__item_has-submenu"><a class="list-top-menu__link" href="#">Gallery</a>
+                            <ul class="list-submenu">
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="gallery-3col-box.html">Gallery 3 Columns Box</a></li>
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="gallery-3col-fullwidth.html">Gallery 3 Columns Fullwidth</a></li>
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="gallery-4col-box.html">Gallery 4 Columns Box</a></li>
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="gallery-4col-fullwidth.html">Gallery 4 Columns Fullwidth</a></li>
+                            </ul>
+                        </li>
+                        <li class="list-top-menu__item list-top-menu__item_has-submenu"><a class="list-top-menu__link" href="#">Team</a>
+                            <ul class="list-submenu">
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="team.html">Our Team</a></li>
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="team-details.html">Member Details</a></li>
+                            </ul>
+                        </li>
+                        <li class="list-top-menu__item"><a class="list-top-menu__link" href="pricing.html">Pricing</a></li>
+                        <li class="list-top-menu__item list-top-menu__item_has-submenu"><a class="list-top-menu__link" href="#">Blog</a>
+                            <ul class="list-submenu">
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="blog.html">Blog List Posts</a></li>
+                                <li class="list-submenu__item"><a class="list-submenu__link" href="blog-singlepost.html">Blog Singlepost</a></li>
+                            </ul>
+                        </li>
+                        <li class="list-top-menu__item"><a class="list-top-menu__link" href="contacts.html">Contact</a></li>
+                    </ul>
+                </div>
+            </div><a class="top-navigation__toggle-menu" id="js-toggle-menu" href="#"></a>
+        </nav>
+    </div>
+    <!-- - nav -->
+</header>
+<!-- - PAGE HEADER -->
